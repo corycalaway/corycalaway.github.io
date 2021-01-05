d5dc2972f373bff5fb78f4efbc67ce9388ebadb6
corycalaway <cory.c.calaway@gmail.com>
2021-01-04 22:43:19 -0700

    working on grid layout

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,3 +4,10 @@ $('#linkWebsiteOne').on('click',function() {
     window.location.href = 'https://corycalaway.github.io/gamer-portal/';
     console.log('test')
 })
+
+// link to led wall
+
+$('#linkWebsiteTwo').on('click',function() {
+    window.location.href = 'https://corycalaway.github.io/gamer-portal/';
+    console.log('test')
+})
