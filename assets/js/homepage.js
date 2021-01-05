75d9f220ac4cd1920b3f17dd9ddef88694acf8b2
corycalaway <cory.c.calaway@gmail.com>
2021-01-04 23:54:35 -0700

    portfolio page operational

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -18,3 +18,15 @@ $('#linkWebsiteThree').on('click',function() {
     window.location.href = 'https://corycalaway.github.io/weather-dashboard/';
     console.log('test')
 })
+
+// link click holder
+
+$('#linkWebsiteFour').on('click',function() {
+    window.location.href = 'https://corycalaway.github.io/under-construction/';
+    console.log('test')
+})
+
+$('#linkWebsiteFive').on('click',function() {
+    window.location.href = 'https://corycalaway.github.io/under-construction/';
+    console.log('test')
+})
