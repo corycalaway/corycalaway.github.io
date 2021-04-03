ee8acee42d7f73ffa88648bbcc6cf4c8aac9c876
Cory Calaway <cory.c.calaway@gmail.com>
2021-04-03 02:06:27 -0600

    email sent when contact filled out

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,41 +1,40 @@
 var express = require('express');
 var router = express.Router();
-// const nodemailer = require('nodemailer')
-// require('dotenv').config();
+ const nodemailer = require('nodemailer')
+require('dotenv').config();
 
 router.post("/", ({body}, res, next) => {
     
-console.log(res)
-res.json(body)
-// console.log(body)
-//   async function emailSend() {
-//     let transporter = nodemailer.createTransport({
-//         host: "smtp.ethereal.email",
-//         port: 587,
-//         secure: false, // true for 465, false for other ports
-//         auth: {
-//             user: process.env.EMAIL, // generated ethereal user
-//             pass: process.env.PASSWORD, // generated ethereal password
-//         },
-//     });
 
-//     let info = await transporter.sendMail({
-//         from: 'cory.c.calaway@gmail.com', // sender address
-//         to: "dane65@ethereal.email", // list of receivers
-//         subject: "Hello ✔", // Subject line
-//         text: "Hello world?", // plain text body
-//         html: "<b>Hello world?</b>", // html body
-//     });
+ console.log(body.name)
+  async function emailSend() {
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.EMAIL, // generated ethereal user
+            pass: process.env.PASSWORD, // generated ethereal password
+        },
+    });
 
-//     transporter.sendMail(info, function (err, data) {
-//         if (err) {
-//             console.log('err')
-//         } else {
-//             console.log('yay')
-//         }
-//     })
-// }
-// emailSend()
+    let info = await transporter.sendMail({
+        from: body.email, // sender address
+        to: "dane65@ethereal.email", // list of receivers
+        subject: body.name, // Subject line
+        text: body.message, // plain text body
+        // html: "<b>Hello world?</b>", // html body
+    });
+
+    transporter.sendMail(info, function (err, data) {
+        if (err) {
+            console.log('err')
+        } else {
+            console.log('yay')
+        }
+    })
+}
+emailSend()
     // Transaction.insertMany(body)
     //   .then(dbTransaction => {
     //     res.json(dbTransaction);
@@ -43,6 +42,9 @@ res.json(body)
     //   .catch(err => {
     //     res.status(404).json(err);
     //   });
+
+    
+res.json(body)
   });
 
 module.exports = router;
