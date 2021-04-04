9759c6904418529fe3a8f4a2734df3466dd16219
Cory Calaway <cory.c.calaway@gmail.com>
2021-04-04 14:31:20 -0600

    build correction

--- src/App.js
+++ src/App.js
@@ -4,24 +4,7 @@ import PortfolioContainer from "./components/PortfolioContainer";
 import Footer from "./components/Footer";
 
 const App = () => {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = { apiResponse: "" };
-    // }
 
-//     const [state, setState] = useState({apiResponse: ""}) ;
-  
-    
-//    function callAPI() {
-//         fetch("http://localhost:9000/testAPI")
-//             .then(res => res.text())
-//             .then(res => setState({ apiResponse: res }));
-//     }
-    
-   
-        
-    
-    
 return (
 <>
 <PortfolioContainer />
